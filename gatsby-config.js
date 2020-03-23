0cefe7b8dda08fd4349a4f98a0f1cbc2a177ef0b
Colin Spence <spencec6@gmail.com>
2020-03-23 17:07:11 -0600

    removes plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
     }
   },
   plugins: [
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
     `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-remove-serviceworker`,
