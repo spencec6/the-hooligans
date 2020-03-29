0d9a9fcbca54bc3c0f9dfcdd1f847577ea4cab3a
Colin Spence <spencec6@gmail.com>
2020-03-29 13:57:30 -0600

    removes bg image

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -1,29 +1,8 @@
 import React from 'react'
 import emotionReset from 'emotion-reset';
 import {Global, css} from '@emotion/core';
-// import backgroundTexture from '../images/background-texture.jpg'
-import { useStaticQuery, graphql } from 'gatsby';
 
 function GlobalStyles() {
-  const data = useStaticQuery(graphql`
-    query {
-      mobileBg: file(relativePath: { eq: "background-texture.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 768, quality: 60) {
-              src
-            }
-          }
-        }
-      desktopBg: file(relativePath: { eq: "background-texture.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 1024, quality: 60) {
-            src
-          }
-        }
-      }
-    }
-  `)
-  const { mobileBg, desktopBg } = data
   return (
     <Global styles={css`
       ${emotionReset}
@@ -34,14 +13,10 @@ function GlobalStyles() {
       }
 
       body { 
-        /* background-image: url(${mobileBg.childImageSharp.fluid.src}); */
         font-family: 'Beth Ellen', cursive;
         line-height: 150%;
         height: 100%;
         overflow-x: hidden;
-        @media only screen and (min-width: 768px) {
-          /* background-image: url(${desktopBg.childImageSharp.fluid.src}); */
-        }
       }
 
       p { 
