28e12f48fbdb9eaf336c360e88b481b492634704
Colin Spence <spencec6@gmail.com>
2020-03-29 23:05:53 -0600

    converts this.state class to useState() hook

--- src/components/RepresentationForm.js
+++ src/components/RepresentationForm.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import React from "react";
+import { useState } from "react";
 import { navigate } from "gatsby-link";
 import { jsx } from 'theme-ui'
 import RoundSmear from "-!svg-react-loader!../images/SVGs/round-smear-1.inline.svg";
@@ -18,155 +18,158 @@ function encode(data) {
   return formData;
 }
 
-export default class RepresentationForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sending: false
-    };
-  }
+const RepresentationForm = () => {
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  const [isSending, setIsSending] = useState(0);
+  const [formState, setFormState] = useState({});
+
+  const handleChange = e => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value
+    });
   };
 
-  handleAttachment = e => {
-    this.setState({ [e.target.name]: e.target.files[0] });
+  const handleAttachment = e => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.files[0]
+    });
   };
-
-  handleSubmit = e => {
+  
+  const handleSubmit = e => {
     e.preventDefault();
     const form = e.target;
-    this.setState({ sending: true })
+    setIsSending(0)
     fetch("/", {
       method: "POST",
       body: encode({
         "form-name": form.getAttribute("name"),
-        ...this.state
+        ...formState
       })
     })
-      .then(() => this.setState({ sending: false }), navigate(form.getAttribute("action")))
+      .then(() => setIsSending(0), navigate(form.getAttribute("action")), console.log(formState))
       .catch(error => alert(error));
   };
 
-  render() {
-    return (
-      <form
-        name="representation"
-        method="POST"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        action="/thank-you"
-        onSubmit={this.handleSubmit}
-        sx={{ width: '100%'}}
-      >
-        <input type="hidden" name="bot-field" onChange={this.handleChange}/>
-        <input type="hidden" name="form-name" value="representation" />
-        <div sx={{ }}>
-          <Label htmlFor="name" required>Your Name</Label>
-          <Input
-            name="name"
-            type="text"
-            index={1}
-            required
-            onChange={this.handleChange}
-            sx={{ transform: `rotate(${-0.5}deg)` }}
-          />
-        </div>
-        <div sx={{ mt: 3 }}>
-          <Label htmlFor="email" required>Your Email</Label>
-          <Input
-            name="email"
-            type="email"
-            index={2}
-            required
-            onChange={this.handleChange}
-            sx={{ transform: `rotate(${0.7}deg)` }}
-          />
-        </div>
-        <div sx={{ mt: 3 }}>
-          <Label htmlFor="why">Why do you want to become a Hoolgain?</Label>
-          <Input
-            as="textarea"
-            name="why"
-            index={3}
-            rows="3"
-            onChange={this.handleChange}
-            sx={{
-              resize: 'vertical',
-              transform: `rotate(${-0.3}deg)`
-            }}
-          />
-        </div>
-        <div sx={{ mt: 3 }}>
-          <Label required htmlFor="resume">
-            Upload Your Resume
-            <span sx={{ color: 'greys.600', fontSize: 0, fontWeight: 'book', letterSpacing: 0, pl: 1, textTransform: 'lowercase' }}>(.jpg, .jpeg, .png, .pdf)</span>
+  return (
+    <form
+      name="representation"
+      method="POST"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      action="/thank-you"
+      onSubmit={(e) => handleSubmit(e)}
+      sx={{ width: '100%'}}
+    >
+      <input type="hidden" name="bot-field" onChange={(e) => handleChange(e)}/>
+      <input type="hidden" name="form-name" value="representation" />
+      <div sx={{ }}>
+        <Label htmlFor="name" required>Your Name</Label>
+        <Input
+          name="name"
+          type="text"
+          index={1}
+          required
+          onChange={(e) => handleChange(e)}
+          sx={{ transform: `rotate(${-0.5}deg)` }}
+        />
+      </div>
+      <div sx={{ mt: 3 }}>
+        <Label htmlFor="email" required>Your Email</Label>
+        <Input
+          name="email"
+          type="email"
+          index={2}
+          required
+          onChange={(e) => handleChange(e)}
+          sx={{ transform: `rotate(${0.7}deg)` }}
+        />
+      </div>
+      <div sx={{ mt: 3 }}>
+        <Label htmlFor="why">Why do you want to become a Hoolgain?</Label>
+        <Input
+          as="textarea"
+          name="why"
+          index={3}
+          rows="3"
+          onChange={(e) => handleChange(e)}
+          sx={{
+            resize: 'vertical',
+            transform: `rotate(${-0.3}deg)`
+          }}
+        />
+      </div>
+      <div sx={{ mt: 3 }}>
+        <Label required htmlFor="resume">
+          Upload Your Resume
+          <span sx={{ color: 'greys.600', fontSize: 0, fontWeight: 'book', letterSpacing: 0, pl: 1, textTransform: 'lowercase' }}>(.jpg, .jpeg, .png, .pdf)</span>
+        </Label>
+        <Input
+          type="file"
+          name="resume"
+          index={4}
+          required
+          accept=".jpg,.png,.pdf,.jpeg"
+          onChange={(e) => handleAttachment(e)}
+          sx={{
+            transform: `rotate(${0.4}deg)`
+          }}
+        />
+      </div>
+      <div sx={{ mt: 3 }}>
+        <Label htmlFor="resume">
+          Upload Your Reel
+          <span sx={{ color: 'greys.600', fontSize: 0, fontWeight: 'book', letterSpacing: 0, pl: 1, textTransform: 'lowercase' }}>(.mp4, .mov, .mpeg)</span>
           </Label>
-          <Input
-            type="file"
-            name="resume"
-            index={4}
-            required
-            accept=".jpg,.png,.pdf,.jpeg"
-            onChange={this.handleAttachment}
-            sx={{
-              transform: `rotate(${0.4}deg)`
-            }}
-          />
-        </div>
-        <div sx={{ mt: 3 }}>
-          <Label htmlFor="resume">
-            Upload Your Reel
-            <span sx={{ color: 'greys.600', fontSize: 0, fontWeight: 'book', letterSpacing: 0, pl: 1, textTransform: 'lowercase' }}>(.mp4, .mov, .mpeg)</span>
-            </Label>
-          <Input
-            type="file"
-            name="resume"
-            index={5}
-            accept=".mp4,.mov,.mpeg"
-            onChange={this.handleAttachment}
-            sx={{
-              transform: `rotate(${-0.6}deg)`
-            }}
-          />
-        </div>
-        <Button type="submit" sx={{ variant: 'buttons.secondary', mt: 3}}>
-          {this.state.sending ? (
-            <div sx={{
-              alignItems: 'center',
-              display: 'flex',
-            }}>
-              {Array.from({ length: 3}).map((_node, index) => {
-                return (
-                  <RoundSmear
-                    key={`dot-${index}`}
-                    className="about-bulletSmear"
-                    sx={{
-                      animation: `${FadeInOut} 1s ${index * 0.25}s infinite`,
-                      color: 'primary',
-                      height: '5px',
-                      ml: index === 0 ? 0 : 3,
-                      transform: `rotate(${randomize(0,360)}deg)`,
-                      width: '5px',
-                    }}
-                  />
-                )
-              })}
-              <div
-                sx={{
-                  color: 'primary',
-                  pl: 3,
-                }}
-              >
-                Sending
-              </div>
+        <Input
+          type="file"
+          name="resume"
+          index={5}
+          accept=".mp4,.mov,.mpeg"
+          onChange={(e) => handleAttachment(e)}
+          sx={{
+            transform: `rotate(${-0.6}deg)`
+          }}
+        />
+      </div>
+      <Button type="submit" sx={{ variant: 'buttons.secondary', mt: 3}}>
+        {isSending ? (
+          <div sx={{
+            alignItems: 'center',
+            display: 'flex',
+          }}>
+            {Array.from({ length: 3}).map((_node, index) => {
+              return (
+                <RoundSmear
+                  key={`dot-${index}`}
+                  className="about-bulletSmear"
+                  sx={{
+                    animation: `${FadeInOut} 1s ${index * 0.25}s infinite`,
+                    color: 'primary',
+                    height: '5px',
+                    ml: index === 0 ? 0 : 3,
+                    transform: `rotate(${randomize(0,360)}deg)`,
+                    width: '5px',
+                  }}
+                />
+              )
+            })}
+            <div
+              sx={{
+                color: 'primary',
+                pl: 3,
+              }}
+            >
+              Sending
             </div>
-          ) : 
-            "Show Us Your Stuff"
-          }
-        </Button>
-      </form>
-    )
-  }
+          </div>
+        ) : 
+          "Show Us Your Stuff"
+        }
+      </Button>
+    </form>
+  )
 }
+
+export default RepresentationForm
